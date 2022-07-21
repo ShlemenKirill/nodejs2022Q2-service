2cdebe13205ecc9fb595035c7758fae33e0906d7
ShlemenKirill <kirill.shlemen@gmail.com>
2022-07-22 01:19:44 +0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { stringify } from 'yaml';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as fs from 'fs';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const options = new DocumentBuilder()
     .setTitle('Home Library')
